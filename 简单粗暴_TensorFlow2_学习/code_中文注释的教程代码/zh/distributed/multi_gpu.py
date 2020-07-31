738d087511406bd93302d778f600dc4d9fedf436
gjy <w2504187153@sina.com>
2020-07-31 12:08:02 +0800

    修改

---
+++ 简单粗暴_TensorFlow2_学习/code_中文注释的教程代码/zh/distributed/multi_gpu.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+num_epochs = 5
+batch_size_per_replica = 64
+learning_rate = 0.001
+
+strategy = tf.distribute.MirroredStrategy()
+print('Number of devices: %d' % strategy.num_replicas_in_sync)  # 输出设备数量
+batch_size = batch_size_per_replica * strategy.num_replicas_in_sync
+
+# 载入数据集并预处理
+def resize(image, label):
+    image = tf.image.resize(image, [224, 224]) / 255.0
+    return image, label
+
+# 使用 TensorFlow Datasets 载入猫狗分类数据集，详见“TensorFlow Datasets数据集载入”一章
+dataset = tfds.load("cats_vs_dogs", split=tfds.Split.TRAIN, as_supervised=True)
+dataset = dataset.map(resize).shuffle(1024).batch(batch_size)
+
+with strategy.scope():
+    model = tf.keras.applications.MobileNetV2(weights=None, classes=2)
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
+        loss=tf.keras.losses.sparse_categorical_crossentropy,
+        metrics=[tf.keras.metrics.sparse_categorical_accuracy]
+    )
+
+model.fit(dataset, epochs=num_epochs)
---
+++ 简单粗暴_TensorFlow2_学习/code_中文注释的教程代码/zh/model/utils.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+import numpy as np
+
+
+class MNISTLoader():
+    def __init__(self):
+        mnist = tf.keras.datasets.mnist
+        (self.train_data, self.train_label), (self.test_data, self.test_label) = mnist.load_data()
+        # MNIST中的图像默认为uint8（0-255的数字）。以下代码将其归一化到0-1之间的浮点数，并在最后增加一维作为颜色通道
+        self.train_data = np.expand_dims(self.train_data.astype(np.float32) / 255.0, axis=-1)      # [60000, 28, 28, 1]
+        self.test_data = np.expand_dims(self.test_data.astype(np.float32) / 255.0, axis=-1)        # [10000, 28, 28, 1]
+        self.train_label = self.train_label.astype(np.int32)    # [60000]
+        self.test_label = self.test_label.astype(np.int32)      # [10000]
+        self.num_train_data, self.num_test_data = self.train_data.shape[0], self.test_data.shape[0]
+
+    def get_batch(self, batch_size):
+        # 从数据集中随机取出batch_size个元素并返回
+        index = np.random.randint(0, np.shape(self.train_data)[0], batch_size)
+        return self.train_data[index, :], self.train_label[index]
+
+
+class SparseCategoricalAccuracy(tf.keras.metrics.Metric):
+    def __init__(self):
+        super().__init__()
+        self.total = self.add_weight(name='total', dtype=tf.int32, initializer=tf.zeros_initializer())
+        self.count = self.add_weight(name='count', dtype=tf.int32, initializer=tf.zeros_initializer())
+
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        values = tf.cast(tf.equal(y_true, tf.argmax(y_pred, axis=-1, output_type=tf.int32)), tf.int32)
+        self.total.assign_add(tf.shape(y_true)[0])
+        self.count.assign_add(tf.reduce_sum(values))
+
+    def result(self):
+        return self.count / self.total
